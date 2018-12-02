0fd34d1e3426953369c5e700d8d9dc4455e22bd0
mastas3 <rpsm90@gmail.com>
2018-12-02 20:45:31 +0200

    Add basic layout react + css

---
+++ client/components/Footer.js
@@ -0,0 +1,10 @@
+import { Layout } from "antd";
+const { Footer } = Layout;
+
+import "../styles/Footer.css";
+
+export default () => (
+    <Footer className="footer">
+        Bears 22 - Chingu Voyage 7
+    </Footer>    
+)
---
+++ client/components/Sidebar.js
@@ -0,0 +1,29 @@
+import { Layout, Menu, Icon } from "antd";
+import "../styles/Sidebar.css";
+const { Sider } = Layout;
+const MenuItem = Menu.Item;
+
+const Sidebar = () => (
+    <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+    >
+        <div className="logo"></div>
+        <Menu theme="dark" mode="inline">
+            <MenuItem key="1">
+                <Icon type="home" />
+                <span className="nav-text">Home</span>
+            </MenuItem>
+            <MenuItem key="2">
+                <Icon type="question" />
+                <span className="nav-text">About</span>
+            </MenuItem>
+            <MenuItem key="3">
+                <Icon type="book" />
+                <span className="nav-text">Contact</span>
+            </MenuItem>
+        </Menu>
+    </Sider>
+)
+
+export default Sidebar;
