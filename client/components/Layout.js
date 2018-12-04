dc7582216129e0ed87bb13a7862918a718e3dd45
mastas3 <rpsm90@gmail.com>
2018-12-04 17:21:43 +0200

    Add loading spinner component

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,36 +1,46 @@
 import React from "react";
 import {Layout} from "antd";
-
 import Head from "./Head";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 import Footer from "./Footer";
+import Loading from "./Loading";
 
 const {Content} = Layout;
 
-export default props => (
-	<Layout>
-		<Head/>
-		<Sidebar/>
-		<Layout style={{
-			height: "100vh"
-		}}
-		>
-			<Header/>
-			<Content style={{
-				margin: "24px 16px 0"
-			}}
-			>
-				<div style={{
-					padding: 24,
-					background: "#fff",
-					minHeight: 360
-				}}
-				>
-					{props.children}
-				</div>
-			</Content>
-			<Footer/>
-		</Layout>
-	</Layout>
-);
+export default class extends React.Component {
+	state = {
+		loading: true,
+	}
+
+	componentDidMount() {
+		this.setState(prevState => ({
+			loading: false,
+		}));
+	}
+
+	render() {
+		const { children } = this.props;
+		const { loading } = this.state;
+
+		if(loading) {
+			return <Loading size="large" />;
+		}
+
+		return (
+			<Layout style={{ minHeight: '100vh' }}>
+				<Head/>
+				<Sidebar/>
+				<Layout>
+					<Header/>
+					<Content style={{margin: "24px 16px 0"}}>
+						<div style={{padding: 24, background: "#fff", minHeight: 360}}>
+							{children}
+						</div>
+					</Content>
+					<Footer/>
+				</Layout>
+			</Layout>
+		);
+	}
+}
