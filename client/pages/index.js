4cebbd0fc135884b844ddec89e0ac9aaff2ff23d
mastas3 <rpsm90@gmail.com>
2018-12-04 17:19:43 +0200

    Move styles folder into static folder

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import Login from "./login";
-
-import "../static/styles/index.css";
+import "../static/styles/index.css"; // eslint-disable-line import/no-unassigned-import
 
 export default () => <Login/>;
