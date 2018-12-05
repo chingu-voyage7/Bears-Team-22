07d0b9aa176564efd5ed3d92abfe168097c9b721
R-Layer <layer@europe.com>
2018-12-05 13:36:07 +0100

    Removed post component

--- server/components/posts/post-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router(); // eslint-disable-line new-cap
-
-const {postGetAll} = require("./post-controllers");
-
-router.get("/post-get-all", postGetAll);
-
-module.exports = router;
