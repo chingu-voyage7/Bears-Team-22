6b42e7b313a48abe2e172640f103c27562c334a4
R-Layer <layer@europe.com>
2018-12-06 11:31:38 +0100

    add first lookup route

---
+++ server/components/lookups/lookups-controllers.js
@@ -0,0 +1,16 @@
+const Post = require("../posts/post-model");
+const {replaceTagNameWithTagId} = require("../utils");
+
+exports.lookupQuestions = async (req, res) => {
+	try {
+		const tags = await replaceTagNameWithTagId(req.body.tags);
+		const questions = await Post.find({isQuestion: true, tags: {$in: tags}}).exec();
+		if (questions) {
+			res.status(200).json(questions);
+		} else {
+			res.status(200).json({message: "No questions yet!"});
+		}
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
