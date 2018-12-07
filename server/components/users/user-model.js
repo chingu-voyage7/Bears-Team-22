af49cf6b4540cd3f2db12a0e5dacabe795b22e7c
R-Layer <layer@europe.com>
2018-12-07 20:01:53 +0100

    add user component

---
+++ server/components/users/user-model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	passwordHash: {
+		type: String,
+		required: true
+	}
+});
+
+userSchema.pre("save", function (next) {
+	try {
+		const hash = bcrypt.hash(this.passwordHash, 10);
+		this.passwordHash = hash;
+		next();
+	} catch (error) {
+		next(error);
+	}
+});
+
+userSchema.methods.isPasswordValid = function (insertedPassword) {
+	return bcrypt.compareSync(insertedPassword, this.passwordHash);
+};
+
+module.exports = mongoose.model("User", userSchema);
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const {promisify} = require("util");
 const mongoose = require("mongoose");
 const express = require("express");
 
+const userRoutes = require("./components/users/user-routes");
 // Setting a few options to remove warnings on feature deprecations.
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useCreateIndex", true);
@@ -19,7 +20,10 @@ promisify(mongoose.connect)("mongodb://mongo:27017/test_db")
 const port = 5000;
 
 const app = express();
+
 app.use(express.json());
+
+app.use(userRoutes);
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
 });
