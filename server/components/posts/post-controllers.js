fe7bb9319f81c3f2dcc145b77eb6fc66c37b58cc
R-Layer <layer@europe.com>
2018-12-07 20:08:03 +0100

    add post-component along with tag model

---
+++ server/components/posts/post-controllers.js
@@ -0,0 +1,37 @@
+const {replaceTagNameWithTagId} = require("../utils");
+const Post = require("./post-model");
+
+exports.postGetAll = async (req, res) => {
+	try {
+		const posts = await Post.find({});
+
+		if (posts.length > 0) {
+			res.status(200).json(posts);
+		} else {
+			res.status(200).json({message: "No posts yet!"});
+		}
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
+
+exports.postCreate = async (req, res) => {
+	try {
+		const tags = await replaceTagNameWithTagId(req.body.tags);
+		const newPost = await new Post({
+			title: req.body.title,
+			body: req.body.body,
+			isQuestion: true,
+			tags,
+			authorId: "5c07a5a54a9d0c0012cd8b35" // Fake mongo id, the real one must come from auth
+		}).save();
+
+		// Update the field replyId of the previous post with the id of the newly generated one
+		if (req.body.repliedToId) {
+			await Post.findByIdAndUpdate(req.body.repliedToId, {$set: {replyId: newPost._id}});
+		}
+		res.status(200).json(newPost);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
