1a1cfddf2aad26a6c4405d8c62cd9dcb42019de0
R-Layer <layer@europe.com>
2018-12-09 10:00:26 +0100

    add session to express app

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const {promisify} = require("util");
 
 const mongoose = require("mongoose");
 const express = require("express");
+const session = require('express-session');
+const mongoSessionStore = require('connect-mongodb-session')(session);
 
 const userRoutes = require("./components/users/user-routes");
 const postRoutes = require("./components/posts/post-routes");const lookupRoutes = require("./components/lookups/lookup-routes");
@@ -19,9 +21,26 @@ promisify(mongoose.connect)("mongodb://mongo:27017/test_db")
 	});
 
 const port = 5000;
+const store = new mongoSessionStore({
+	uri: "mongodb://mongo:27017/test_db_sessions",
+	collection: 'knowledge_sessions'
+});
+
+store.on('error', error => {
+	console.log('err up', error);		// handle it properly
+})
+const sessionOptions = {
+	secret: 'WannaBeASecret!',
+	resave: false,
+	saveUninitialized: false,
+	cookie: { maxAge: 60*60*1000 },
+	store: store
+	// secure: true - As per doc, is recommended but it needs a https connection^^
+}
 
 const app = express();
 
+app.use(session(sessionOptions));
 app.use(express.json());
 app.use(userRoutes);
 app.use(postRoutes);
