77b067ac0e25998e79c1950740877cff60bf9fc6
Itai Steinherz <itaisteinherz@gmail.com>
2018-12-09 13:27:51 +0200

    Revert “add user component”
    This reverts af49cf6b4540cd3f2db12a0e5dacabe795b22e7c.

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ const {promisify} = require("util");
 const mongoose = require("mongoose");
 const express = require("express");
 
-const userRoutes = require("./components/users/user-routes");
 const postRoutes = require("./components/posts/post-routes");
+
 // Setting a few options to remove warnings on feature deprecations.
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useCreateIndex", true);
@@ -23,7 +23,6 @@ const port = 5000;
 const app = express();
 
 app.use(express.json());
-app.use(userRoutes);
 app.use(postRoutes);
 
 app.listen(port, () => {
