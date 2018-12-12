d177917fd88f6b221bddfc7f72e201c1981ad9df
Vincent <10290348+Kagerjay@users.noreply.github.com>
2018-12-11 22:06:26 -0500

    initialize thread question/answer page

---
+++ client/pages/thread.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MainLayout from "../components/MainLayout";
+
+class Thread extends React.Component {
+	render() {
+		return (
+			<MainLayout>
+				<div>Hello World</div>
+			</MainLayout>
+		);
+	}
+}
+
+export default Thread;
