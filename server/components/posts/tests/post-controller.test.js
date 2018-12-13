f551c413227ca3bb5794623de4e6de906b312a09
R-Layer <layer@europe.com>
2018-12-13 10:38:19 +0100

    add post component tests

---
+++ server/components/posts/tests/post-controller.test.js
@@ -0,0 +1,82 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+
+const {expect} = chai;
+const mongoose = require("mongoose");
+
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useCreateIndex", true);
+
+const app = require("../../../app");
+
+chai.use(chaiHttp);
+
+describe(" -- POST ROUTES -- ", () => {
+	afterEach(() => {
+		try {
+			// Mongoose.connection.db.dropCollection("posts")
+		} catch (error) {
+			// Silently fail if user collection doesn't exist
+		}
+	});
+
+	describe("GET /post-get-all", () => {
+		it("should hit GET /post-get-all", done => {
+			chai.request(app)
+				.get("/post-get-all")
+				.end((err, res) => {
+					// eslint-disable-next-line no-unused-expressions
+					expect(err).to.be.null;
+					expect(res).to.have.status(200);
+					done();
+				});
+		});
+
+		it("should return all the registered posts", done => {
+			chai.request(app)
+				.get("/post-get-all")
+				.end((err, res) => {
+					// eslint-disable-next-line no-unused-expressions
+					expect(err).to.be.null;
+					expect(res).to.have.status(200);
+					expect(res.body).to.be.an("array").to.have.lengthOf(0);
+					done();
+				});
+		});
+	});
+
+	describe("POST /post-create", () => {
+		it("should hit POST /post-create", done => {
+			chai.request(app)
+				.post("/post-create")
+				.end((err, res) => {
+					// eslint-disable-next-line no-unused-expressions
+					expect(err).to.be.null;
+					/*  The route is being hit but it thrown error
+					 because of no body provided */
+					expect(res).to.have.status(500);
+					done();
+				});
+		});
+
+		it("should create a post", done => {
+			chai.request(app)
+				.post("/post-create")
+				.send({
+					title: "myFirstPost",
+					body: "Hey, this is my first post! :D",
+					isQuestion: false,
+					tags: ["testTag", "testTag2"],
+					authorId: "5c07a5a54a9d0c0012cd8b35"
+				})
+				.end((err, res) => {
+					// eslint-disable-next-line no-unused-expressions
+					expect(err).to.be.null;
+					expect(res).to.have.status(201);
+					expect(res.body.title).to.equal("myFirstPost");
+					expect(res.body.body).to.equal("Hey, this is my first post! :D");
+					done();
+				});
+		});
+	});
+});
---
+++ server/components/posts/tests/post-model.test.js
@@ -0,0 +1,62 @@
+const {expect} = require("chai");
+const Post = require("../post-model");
+const {replaceTagNameWithTagId} = require("../../utils");
+
+console.log("post-model");
+describe(" -- POST MODEL -- ", () => {
+	let mockPost;
+	beforeEach(() => {
+		mockPost = {
+			title: "myFirstPost",
+			body: "Hey, this is my first post! :D",
+			tags: ["testTag", "testTag2"],
+			authorId: "5c07a5a54a9d0c0012cd8b35"
+		};
+	});
+
+	it("should replace the tag names with the tag ids", async () => {
+		const mockTagArray = ["test1", "test2"];
+		const mockIdArray = await replaceTagNameWithTagId(mockTagArray);
+		expect(mockIdArray).to.have.lengthOf(2);
+	});
+
+	it("should be rejected if no title is provided", async () => {
+		delete mockPost.title;
+		const tagIds = await replaceTagNameWithTagId(mockPost.tags);
+		mockPost.tags = tagIds;
+		const newPost = new Post(mockPost);
+		await newPost.validate(err => {
+			const arrErrors = Object.keys(err.errors);
+			expect(arrErrors).to.have.lengthOf(1);
+			expect(arrErrors).to.include("title");
+			expect(err.errors.title.message).to.equal("Path `title` is required.");
+		});
+	});
+
+	it("should be rejected if no body is provided", async () => {
+		delete mockPost.body;
+		const tagIds = await replaceTagNameWithTagId(mockPost.tags);
+		mockPost.tags = tagIds;
+		const newPost = new Post(mockPost);
+		newPost.validate(err => {
+			const arrErrors = Object.keys(err.errors);
+			expect(arrErrors).to.have.lengthOf(1);
+			expect(arrErrors).to.include("body");
+			expect(err.errors.body.message).to.equal("Path `body` is required.");
+		});
+	});
+
+	it("should be rejected if no authorId is provided", async () => {
+		delete mockPost.authorId;
+		const tagIds = await replaceTagNameWithTagId(mockPost.tags);
+		mockPost.tags = tagIds;
+		const newPost = new Post(mockPost);
+		newPost.validate(err => {
+			const arrErrors = Object.keys(err.errors);
+			expect(arrErrors).to.have.lengthOf(1);
+			expect(arrErrors).to.include("authorId");
+			expect(err.errors.authorId.message).to.equal("Path `authorId` is required.");
+		});
+	});
+});
+
