1d0891020b5431581ca03ef43124e9ebb10f6e59
Vincent <10290348+Kagerjay@users.noreply.github.com>
2018-12-13 23:09:02 -0500

    Rendering several simple responses to question template

--- client/pages/thread.js
+++ client/pages/thread.js
@@ -1,14 +1,16 @@
 import React from "react";
 import MainLayout from "../components/MainLayout";
-import Question from "../components/Answer";
-import Answer from "../components/Question";
+import Answer from "../components/Answer";
+import Question from "../components/Question";
 class Thread extends React.Component {
 	render() {
-		// TODO - fix items loading in reverse order from MainLayout
+		const Tester = () => <div>Yo hey what's up dude</div>;
 		return (
 			<MainLayout>
-				<Answer />
 				<Question />
+				{[1, 2, 3, 4, 5, 6].map((value, index) => {
+					return <Answer key={index}>{value}</Answer>;
+				})}
 			</MainLayout>
 		);
 	}
