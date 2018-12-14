29d3eef814a255a235c1af063195f30820155795
Itai Steinherz <itaisteinherz@gmail.com>
2018-12-14 20:34:40 +0200

    Rename login method to `loginUser`

--- server/components/users/user-controllers.js
+++ server/components/users/user-controllers.js
@@ -21,17 +21,17 @@ exports.registerUser = async (req, res) => {
 			email: req.body.email,
 			passwordHash: req.body.password
 		}).save();
-		
+
 		res.status(201).json({newUser});
 	} catch (error) {
 		res.status(500).json({error});
 	}
 };
 
-exports.loginUserFake = async (req, res) => {
+exports.loginUser = async (req, res) => {
 	try {
 		const user = await User.findOne({name: req.body.name});
-		
+
 		if (user && user.validatePassword(req.body.password)) {
 			res.status(200).json({message: "Login successful"});
 		} else {
--- server/components/users/user-routes.js
+++ server/components/users/user-routes.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const router = express.Router(); // eslint-disable-line new-cap
 
-const {getAllUsers, registerUser, loginUserFake} = require("./user-controllers");
+const {getAllUsers, registerUser, loginUser} = require("./user-controllers");
 
 router.get("/user/get-all", getAllUsers);
 router.post("/user/register", registerUser);
-router.post("/user/login", loginUserFake);
+router.post("/user/login", loginUser);
 
 module.exports = router;
