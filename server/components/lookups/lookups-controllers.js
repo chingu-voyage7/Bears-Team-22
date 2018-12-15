ba1159647a419ef9b55517afb7092509c28baa75
R-Layer <layer@europe.com>
2018-12-15 17:50:05 +0100

    add thread lookup

--- server/components/lookups/lookups-controllers.js
+++ server/components/lookups/lookups-controllers.js
@@ -10,3 +10,20 @@ exports.lookupQuestions = async (req, res) => {
 		res.status(500).json(error);
 	}
 };
+
+exports.lookupThread = async (req, res) => {
+	const thread = [];
+	let actualPost = {};
+	let actualId = req.params.id;
+	do {
+		try {
+			actualPost = await Post.findById(actualId).exec();
+			actualId = actualPost.replyId;
+			thread.push(actualPost);
+		} catch(error) {
+			res.status(500).json(error);
+		}
+	} while (actualPost.replyId);
+	
+	res.status(200).json(thread);
+};
--- server/components/lookups/tests/lookups.test.js
+++ server/components/lookups/tests/lookups.test.js
@@ -16,28 +16,40 @@ chai.use(chaiHttp);
 describe(" -- LOOKUP ROUTES -- ", () => {
 	before(async () => {
 		const firstPost = {
+			_id: "5c152073f01305001307a3ad",
 			title: "myFirstPost",
 			body: "Hey, this is my first post! :D",
-			isQuestion: false,
+			isQuestion: true,
 			tags: ["testTag", "testTag2"],
-			authorId: "5c07a5a54a9d0c0012cd8b35"
+			authorId: "5c07a5a54a9d0c0012cd8b35",
+			replyId: "5c151ffaf01305001307a3ab"
 		};
 
 		const secondPost = {
-			title: "mySecondPost",
+			_id: "5c151ffaf01305001307a3ab",
+			//replyId: "5c1520faf01305001307a3ae",
 			body: "Hey, this is my second post! :D",
-			isQuestion: false,
 			tags: ["testTag2", "testTag3"],
+			authorId: "5c07a5a54a9d0c0012cd8b35"
+		};
+
+		const thirdPost = {
+			_id: "5c152045f01305001307a3ac",
+			body: "Hey, this is my third post! :D",
+			tags: ["testTag3", "testTag4"],
 			authorId: "5c07a5a54a9d0c0012cd8b35"
 		};
 
 		const firstPostTags = await replaceTagNameWithTagId(firstPost.tags);
 		const secondPostTags = await replaceTagNameWithTagId(secondPost.tags);
+		const thirdPostTags = await replaceTagNameWithTagId(thirdPost.tags);
 		firstPost.tags = firstPostTags;
 		secondPost.tags = secondPostTags;
+		thirdPost.tags = thirdPostTags;
 
 		new Post(firstPost).save();
 		new Post(secondPost).save();
+		new Post(thirdPost).save();
 	});
 	after(() => {
 		mongoose.connection.db.dropCollection("posts");
@@ -45,10 +57,10 @@ describe(" -- LOOKUP ROUTES -- ", () => {
 		mongoose.connection.db.dropCollection("tags");
 	});
 
-	describe("POST /lookup-questions", () => {
-		it("should hit POST /lookup-questions", done => {
+	describe("POST /lookup/questions", () => {
+		it("should hit POST /lookup/questions", done => {
 			chai.request(app)
-				.post("/lookup-questions")
+				.post("/lookup/questions")
 				.end((err, res) => {
 					// eslint-disable-next-line no-unused-expressions
 					expect(err).to.be.null;
@@ -60,7 +72,7 @@ describe(" -- LOOKUP ROUTES -- ", () => {
 
 		it("should retrieve posts based on the tags provided", done => {
 			chai.request(app)
-				.post("/lookup-questions")
+				.post("/lookup/questions")
 				.send({tags: ["testTag2"]})
 				.end((err, res) => {
 					// eslint-disable-next-line no-unused-expressions
@@ -70,4 +82,30 @@ describe(" -- LOOKUP ROUTES -- ", () => {
 				});
 		});
 	});
+
+	describe("GET /lookup/thread", () => {
+		it("should hit GET /lookup/thread", done => {
+			chai.request(app)
+				.get("/lookup/thread/5c152073f01305001307a3ad")
+				.end((err, res) => {
+					// eslint-disable-next-line no-unused-expressions
+					expect(err).to.be.null;
+					// Hit route but no body provided
+					expect(res).to.have.status(200);
+					done();
+				});
+		});
+
+		it("should retrieve posts based on the id of the question", done => {
+			chai.request(app)
+				.get("/lookup/thread/5c152073f01305001307a3ad")
+				.end((err, res) => {
+					// eslint-disable-next-line no-unused-expressions
+					expect(err).to.be.null;
+					expect(res).to.have.status(200);
+					done();
+				});
+		});
+	});
+
 });
