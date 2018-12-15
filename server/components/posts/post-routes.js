165262e36a65f52def21c0b8b6c0a351675a7211
Itai Steinherz <itaisteinherz@gmail.com>
2018-12-15 13:21:20 +0200

    Mount router to path “/post”

--- server/components/posts/post-routes.js
+++ server/components/posts/post-routes.js
@@ -4,7 +4,7 @@ const {getAllPosts, createPost} = require("./post-controllers");
 
 const router = express.Router(); // eslint-disable-line new-cap
 
-router.get("/post/get-all", getAllPosts);
-router.post("/post/create", createPost);
+router.get("/get-all", getAllPosts);
+router.post("/create", createPost);
 
 module.exports = router;
