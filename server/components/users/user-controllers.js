bc9e71bf2098de7763e9313eaa2e5836cf5ae993
Itai Steinherz <itaisteinherz@gmail.com>
2018-12-18 15:08:06 +0200

    Remove `/user/get-all` endpoint

--- server/components/users/user-controllers.js
+++ server/components/users/user-controllers.js
@@ -1,19 +1,5 @@
 const User = require("./user-model");
 
-exports.getAllUsers = async (req, res) => {
-	try {
-		const users = await User.find({});
-
-		if (users.length > 0) {
-			res.status(200).json({users});
-		} else {
-			res.status(200).json({message: "No users yet!"});
-		}
-	} catch (error) {
-		res.status(500).json({error});
-	}
-};
-
 exports.registerUser = async (req, res) => {
 	try {
 		const newUser = await new User({
--- server/components/users/user-routes.js
+++ server/components/users/user-routes.js
@@ -2,9 +2,8 @@ const express = require("express");
 
 const router = express.Router(); // eslint-disable-line new-cap
 
-const {getAllUsers, registerUser, loginUser} = require("./user-controllers");
+const {registerUser, loginUser} = require("./user-controllers");
 
-router.get("/get-all", getAllUsers);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
