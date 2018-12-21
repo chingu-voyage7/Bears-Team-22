ea4456903ac523985aac3a16d103e76616300aba
Vincent <10290348+Kagerjay@users.noreply.github.com>
2018-12-20 23:09:12 -0500

    added more hackernews api state doc sample

--- client/pages/thread.js
+++ client/pages/thread.js
@@ -23,6 +23,7 @@ class Thread extends React.Component {
 			2921983: {
 				by: "norvig",
 				id: 2921983,
+				parent: 2921506,
 				kids: [2922097, 2922429, 2924562, 2922709, 2922573, 2922140, 2922141],
 				text:
 					"Aw shucks, guys ... you make me blush with your compliments.<p>Tell you what, Ill make a deal: I'll keep writing if you keep reading. K?",
@@ -32,6 +33,8 @@ class Thread extends React.Component {
 			35156123: {
 				by: "vincentntang",
 				id: 35156123,
+				parent: 2921506,
+				kids: [2922097, 2922429, 2924562, 2922709, 2922573, 2922140, 2922141],
 				text: "Look at me ma I'm a comment!",
 				time: 131411555,
 				// use unix based time
