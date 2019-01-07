6af1ea6a5b370f90c3920e5de3859e56dfec3ac0
R-Layer <layer@europe.com>
2019-01-07 18:00:03 +0100

    Add thread component (and less basic mock data)

--- server/components/search/search-controller.js
+++ server/components/search/search-controller.js
@@ -1,4 +1,5 @@
-const {Question} = require("../content/content-model");
+const {Question, Reply} = require("../content/content-model");
+const Thread = require("../thread/thread-model");
 const User = require("../users/user-model");
 const mongoose = require("mongoose");
 
@@ -26,27 +27,44 @@ exports.getQuestion = async (req, res) => {
 };
 
 exports.prepopulate = async (req, res, next) => {
-	try {
-		await User.deleteMany({});
-		await Question.deleteMany({});
+    try {
+        await User.deleteMany({});
+        await Question.deleteMany({});
+        await Reply.deleteMany({});
+        await Thread.deleteMany({});
 
-		const user = await User.create({name: "test", email: "test@test.com"});
-		await Question.create([
-			{title: "This is the first title", body: "This is the first body", authorId: mongoose.Types.ObjectId(user._id)},
-			{title: "This is the second title", body: "This is the second body", authorId: mongoose.Types.ObjectId(user._id)},
-			{title: "This is the third title", body: "This is the third body", authorId: mongoose.Types.ObjectId(user._id)},
-			{title: "This is the fourth title", body: "This is the fourth body", authorId: mongoose.Types.ObjectId(user._id)}
-		]);
-		/*  - Just write down the documents you want to insert
-        await xxxx.create([
-            {...},
-            ...
+        let frank = await  User.create({name: "frank", email:"frank@test.com"});
+        let jhon = await  User.create({name: "jhon", email:"jhon@test.com"});
+        let questions = await Question.create([
+            {title: "This is the first title", body: "This is the first body", authorId: mongoose.Types.ObjectId(frank._id)},
+            {title: "This is the second title", body: "This is the second body", authorId: mongoose.Types.ObjectId(frank._id)},
+            {title: "This is the third title", body: "This is the third body", authorId: mongoose.Types.ObjectId(jhon._id)},
+            {title: "This is the fourth title", body: "This is the fourth body", authorId: mongoose.Types.ObjectId(frank._id)}
+        ]); 
+
+        let replies = await Reply.create([
+            {body: "This is the first reply to the first question", authorId: mongoose.Types.ObjectId(jhon._id), questionId: mongoose.Types.ObjectId(questions[0]._id)},
+            {body: "This is the first reply to the second question", authorId: mongoose.Types.ObjectId(jhon._id), questionId: mongoose.Types.ObjectId(questions[1]._id)},
+            {body: "This is the first reply to the third question", authorId: mongoose.Types.ObjectId(frank._id), questionId: mongoose.Types.ObjectId(questions[2]._id)},
+            {body: "This is the second reply to the first question", authorId: mongoose.Types.ObjectId(frank._id), questionId: mongoose.Types.ObjectId(questions[0]._id)},
+            {body: "This is the third reply to the first question", authorId: mongoose.Types.ObjectId(jhon._id), questionId: mongoose.Types.ObjectId(questions[0]._id)},
+            {body: "This is the second reply to the third question", authorId: mongoose.Types.ObjectId(jhon._id), questionId: mongoose.Types.ObjectId(questions[2]._id)},
         ]);
-        */
-		res.status(200).json({message: "Db Populated"});
-		next();
-	} catch (error) {
-		res.status(500).json(error);
-		next();
-	}
-};
+        
+        const threads = [];
+        questions.forEach(question => {
+            let thread = {
+                question: question._id, 
+                replies: replies.filter(reply => reply.questionId == question._id).map(el =>  el._id)};
+            threads.push(thread);
+        });
+
+        await Thread.create(threads);
+        console.log("questions", questions);
+
+        res.status(200).json({message: "Db Populated"})
+    } catch(err) {
+        console.log(err);
+        res.status(500).json(err)
+    }
+}
