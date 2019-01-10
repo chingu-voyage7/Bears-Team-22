3373bf987237dea3258bf432477b1fe3e219f3fa
Vincent <10290348+Kagerjay@users.noreply.github.com>
2019-01-09 21:17:15 -0500

    incorpoated more mock data, removed replyId as extraneous

--- client/sample/data-thread.js
+++ client/sample/data-thread.js
@@ -44,9 +44,27 @@
     updatedAt: "2017-10-02",
     body: "lorem ipsum lorem ipsum",
     authorId: 8863,
-    replyId: "1115555",
+    // replyId: "1115555",
     title: "My YC app: Dropbox - Throw away your USB drive",
     tags: ["ycombinator", "dropbox"]
+  },
+  8864: {
+    createdAt: "2016-05-15",
+    updatedAt: "2016-10-02",
+    body: "lorem ipsum lorem ipsum",
+    authorId: 8863,
+    // replyId: "1115555",
+    title: "My first hello world app!",
+    tags: ["programming", "javascript"]
+  },
+  8866: {
+    createdAt: "2018-05-15",
+    updatedAt: "2018-10-02",
+    body: "lorem ipsum lorem ipsum",
+    authorId: 8863,
+    // replyId: "1115555",
+    title: "I made a thing",
+    tags: ["programming", "awesome"]
   }
 },
 
@@ -54,9 +72,33 @@ export const reply = {
   2921983: {
     createdAt: "2015-05-15",
     updatedAt: "2017-10-02",
-    body: "lorem ipsum lorem ipsum",
-    authorId: 8863,
-    replyId: "1115555",
-    questionId: "111125"
+    body: "lorem ipsum lorem ipsum"1,
+    authorId: 555123,
+    // replyId: "1115555",
+    questionId: 8863
+  },
+  2921984: {
+    createdAt: "2015-05-15",
+    updatedAt: "2017-10-02",
+    body: "lorem ipsum lorem ipsum2",
+    authorId: 555124,
+    // replyId: "1115555",
+    questionId: 8863
+  },
+  2921985: {
+    createdAt: "2015-05-15",
+    updatedAt: "2017-10-02",
+    body: "lorem ipsum lorem ipsum3",
+    authorId: 555125,
+    // replyId: "1115555",
+    questionId: 8863
+  },
+  2921986: {
+    createdAt: "2015-05-15",
+    updatedAt: "2017-10-02",
+    body: "lorem ipsum lorem ipsum3",
+    authorId: 555126,
+    // replyId: "1115555",
+    questionId: 8864
   }
 }
