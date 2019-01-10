8578fd9140067ca986bca997b224a755f303dea1
Vincent <10290348+Kagerjay@users.noreply.github.com>
2019-01-09 21:37:47 -0500

    Clean up data

--- client/pages/thread.js
+++ client/pages/thread.js
@@ -5,8 +5,6 @@ import Question from "../components/Question";
 import { sampleQuestions, sampleReplies } from "../sample/data-thread";
 
 class Thread extends React.Component {
-	// Add state just for thread, may change down road
-	// State should represent the entire tree of comments in nested json format
 	state = {
 		questions: {},
 		replies: {}
@@ -33,7 +31,3 @@ class Thread extends React.Component {
 }
 
 export default Thread;
-
-// {[1, 2, 3, 4, 5, 6].map((value, index) => {
-// 	return <Answer key={index}>{value}</Answer>;
-// })}
