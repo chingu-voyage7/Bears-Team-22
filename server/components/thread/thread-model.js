4e61aebd223c95600bbf10bb1dacf21a2492aaa8
R-Layer <layer@europe.com>
2019-01-10 10:34:21 +0100

    add thread end-point

--- server/components/thread/thread-model.js
+++ server/components/thread/thread-model.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const threadSchema = new mongoose.Schema({
 	question: {
 		type: mongoose.Schema.Types.ObjectId,
-		ref: "Content"
+		ref: 'Content'
 	},
 	replies: [{
 		type: mongoose.Schema.Types.ObjectId,
-		ref: "Content"
+		ref: 'Content'
 	}]
 });
 
-module.exports = mongoose.model("Thread", threadSchema);
+module.exports = mongoose.model('Thread', threadSchema);
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 
 const userRoutes = require('./components/users/user-routes');
 const authRoutes = require('./components/auth/auth-routes');
+const threadRoutes = require('./components/thread/thread-routes');
 const searchRoutes = require('./components/search/search-routes');
 const serviceAccount = require('./stuff.json');
 
@@ -43,9 +44,11 @@ app.use((req, res, next) => {
 
 app.use(cookieParser('this_is_a_secret'));
 app.use(express.json());
-// App.use(csrf());  <-- TODO
+
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
+app.use('/thread', threadRoutes);
+app.use(searchRoutes);
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
