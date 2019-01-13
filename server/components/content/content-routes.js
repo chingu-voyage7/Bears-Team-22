1d121e7b5abbd4917225a809d16dec550f3513e0
R-Layer <layer@europe.com>
2019-01-13 19:52:51 +0100

    implement validation/sanitization

--- server/components/content/content-routes.js
+++ server/components/content/content-routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
+
+const  {validate}  = require('../validation/validationSchemas');
+const  {checkValidation}  = require('../validation/validationMw');
 const {sessionVerificationMw} = require("../auth/auth-controllers");
 const {createContent} = require("./content-controller");
 
 const router = express.Router(); // eslint-disable-line new-cap
 
-router.post("/create", sessionVerificationMw, createContent);
+router.post("/create", validate('post-question'), checkValidation, sessionVerificationMw, createContent);
 module.exports = router;
---
+++ server/components/validation/validationMw.js
@@ -0,0 +1,9 @@
+const { validationResult } = require('express-validator/check');
+
+exports.checkValidation = (req, res, next) => {
+    if(!validationResult(req).isEmpty()) {
+        const validationErrors = validationResult(req).array();
+        return res.status(422).json({validationErrors});
+    }
+    next();
+}
