2afb3a84bd87c9c8406f35e72e2a2c544edbbef6
R-Layer <layer@europe.com>
2019-01-18 11:50:52 +0100

    add validation on create content route
    # Conflicts:
    #	server/components/validation/validation-schemas.js

--- server/components/content/content-routes.js
+++ server/components/content/content-routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
-const {validate} = require("../validation/validation-schemas");
-const {checkValidation} = require("../validation/validation-mw");
+const validator = require("../validation/validation-mw");
 const {sessionVerificationMw} = require("../auth/auth-controllers");
 const {createContent} = require("./content-controller");
 
 const router = express.Router(); // eslint-disable-line new-cap
 
-router.post("/create", validate("post-question"), checkValidation, sessionVerificationMw, createContent);
+router.post("/create",
+	validator,
+	sessionVerificationMw,
+	createContent);
 module.exports = router;
