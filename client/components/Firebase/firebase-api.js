f644cca5be8c57bde6ba11a5a2af530eb5d2b94a
Itai Steinherz <itaisteinherz@gmail.com>
2019-01-19 15:18:35 +0200

    Fix CI build and improve getting started guide

--- client/components/Firebase/firebase-api.js
+++ client/components/Firebase/firebase-api.js
@@ -1,5 +1,5 @@
 import firebase from "firebase/app";
 
-import clientCredentials from "../../credentials/client";
+import clientCredentials from "../../client-cred";
 
 export default firebase.apps.length > 0 ? firebase.app() : firebase.initializeApp(clientCredentials);
