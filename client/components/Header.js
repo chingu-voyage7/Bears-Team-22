10c93f508224f2cb4409e7919c817ded17020698
George Keloglou <geokeloglou@gmail.com>
2019-01-27 18:43:53 +0200

    refactored but still issue

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,55 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
 import {Layout, Menu, Icon, Drawer} from "antd";
-
 import "../static/styles/Header.css";
 
 const {Header} = Layout;
-const MenuItem = Menu.Item;
-
-const MainMenu = ({mode = "horizontal", className = "header__menu", theme = "light", authState = "unchecked"}) => (
-	<Menu theme={theme} mode={mode} className={className}>
-		<div className="logo"/>
-		{authState !== "unchecked" ? // eslint-disable-line no-negated-condition
-			<MenuItem key="1">
-				<span className="nav-text">
-					<Link href={`/${authState === "logged in" ? "logout" : "login"}`}>
-						<a>{authState === "logged in" ? "Logout" : "Login"}</a>
-					</Link>
-				</span>
-			</MenuItem> :
-			null}
-		{authState === "logged out" ?
-			<MenuItem key="2">
-				<span className="nav-text">
-					<Link href="/register">
-						<a>Register</a>
-					</Link>
-				</span>
-			</MenuItem> :
-			null}
-	</Menu>
-);
-
-MainMenu.propTypes = {
-	mode: PropTypes.string,
-	className: PropTypes.string,
-	theme: PropTypes.string,
-	authState: PropTypes.string
-};
-
-MainMenu.defaultProps = {
-	mode: "horizontal",
-	className: "header__menu",
-	theme: "light",
-	authState: "unchecked"
-};
 
 const MainDrawer = ({drawerIsVisible, onClose}) => {
 	return (
 		<div>
 			<Drawer visible={drawerIsVisible} onClose={onClose}>
-				<MainMenu mode="vertical" className="mobile__menu"/>
+				<Menu mode="vertical" className="mobile__menu"/>
 			</Drawer>
 		</div>
 	);
@@ -62,9 +22,20 @@ MainDrawer.propTypes = {
 };
 
 export default class MainHeader extends React.Component {
+	
 	state = {
-		drawerIsVisible: false
+		drawerIsVisible: false,
+		current: '0',
 	};
+
+  handleClick = (e) => {
+		console.log('click ', e);
+		console.log(this.state.current, 'Before setState');
+    this.setState({
+			current: e.key,
+		});
+		console.log(this.state.current, 'After setState');
+  }
 
 	openDrawer = () => {
 		this.setState({
@@ -90,7 +61,29 @@ export default class MainHeader extends React.Component {
 							<a>Knowledge</a>
 						</Link>
 					</div>
-					<MainMenu authState={authState}/>
+					<Menu
+						onClick={this.handleClick}
+						selectedKeys={[this.state.current]}
+						authState={authState} 
+						mode="horizontal" 
+						className="header__menu" 
+						theme="light"
+					>
+						{authState !== "unchecked" ? // eslint-disable-line no-negated-condition
+							<Menu.Item key="1">
+								<Link href={`/${authState === "logged in" ? "logout" : "login"}`}>
+									<a>{authState === "logged in" ? "Logout" : "Login"}</a>
+								</Link>
+							</Menu.Item> :
+							null}
+						{authState === "logged out" ?
+							<Menu.Item key="2">
+								<Link href="/register">
+									<a>Register</a>
+								</Link>
+							</Menu.Item> :
+							null}
+					</Menu>
 					<div className="header__drawer--toggle" onClick={this.openDrawer}>
 						<Icon type="bars" onClick={this.openDrawer}/>
 					</div>
@@ -100,6 +93,20 @@ export default class MainHeader extends React.Component {
 		);
 	}
 }
+
+Menu.defaultProps = {
+	mode: "horizontal",
+	className: "header__menu",
+	theme: "light",
+	authState: "unchecked"
+};
+
+Menu.propTypes = {
+	mode: PropTypes.string,
+	className: PropTypes.string,
+	theme: PropTypes.string,
+	authState: PropTypes.string
+};
 
 MainHeader.propTypes = {
 	authState: PropTypes.string.isRequired
