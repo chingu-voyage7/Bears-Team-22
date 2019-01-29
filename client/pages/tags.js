af2179b9e199cbb44ee1d62fc22d635ced8d17e3
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2019-01-28 20:29:29 -0500

    basic main layout added

--- client/pages/tags.js
+++ client/pages/tags.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { taggedQuestions} from '../sample/tags-data';
 import "../static/styles/Tags.css";
+import MainLayout from "../components/MainLayout";
 
 class Tags extends React.Component {
   render() {
     return (
-      <div>
-        Hello world
-      </div>
+      <MainLayout>
+        <div>hello world</div>
+			</MainLayout>
     );
   }
 }
