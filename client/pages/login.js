3a461b2b0a0bc5226306296f216552ca56d0b771
R-Layer <layer@europe.com>
2019-02-01 16:38:35 +0100

    refactor login error notification

--- client/pages/login.js
+++ client/pages/login.js
@@ -13,28 +13,37 @@ import "isomorphic-unfetch";
 class Login extends React.Component {
 	login = async (email, password) => {
 		// Since we will use sessions the token auth will be saved and removed
+
+		const fetchOpts = {
+			method: "POST",
+			headers: new Headers({"Content-Type": "application/json"}),
+			credentials: "include",
+		};
+
 		try {
 			firebase.auth().setPersistence(auth.Auth.Persistence.NONE);
 			const userData = await firebase.auth().signInWithEmailAndPassword(email, password);
 			const idToken = await userData.user.getIdToken();
+			fetchOpts.body = JSON.stringify({idToken});
+		} catch (error) {
+			const errorCode = error.code;
+  			if (errorCode === 'auth/wrong-password' || errorCode === 'auth/user-not-found') {
+				return { message: "Invalid credentials"};
+			} else {
+				return { message: "Login failed"};
+			}
+		}
 
-			// Const _csrf = Cookies.get('XSRF-TOKEN');  <-- TODO
-			const fetchOpts = {
-				method: "POST",
-				headers: new Headers({"Content-Type": "application/json"}),
-				credentials: "include",
-				body: JSON.stringify({idToken})
-			};
-
+		try {
 			// When a user login it makes a call to the server
 			// to set the cookie and sync mongo with firebase
 			await fetch("http://localhost:5000/auth/login", fetchOpts);
 			await firebase.auth().signOut(); // Cookies are set, no need to mantain the token in the storage
-
 			Router.push("/");
 			return null;
 		} catch (error) {
-			return error;
+			await fetch("http://localhost:5000/auth/logout");   // User already logged in with FB
+			return { message: "Internal server error"};
 		}
 	};
 
